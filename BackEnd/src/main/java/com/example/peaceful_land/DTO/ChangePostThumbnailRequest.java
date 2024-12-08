d72f0da33c6425d901458911cb5c4c511eecd074
DangTrong04 <dangvantrong2004@gmail.com>
2024-12-08 17:15:07 +0700

    tất cả chức năng cập nhật bài đăng (trừ thằng giảm giá ra), có thời gian thì làm sau.

--- BackEnd/src/main/java/com/example/peaceful_land/DTO/ChangePostThumbnailRequest.java
+++ BackEnd/src/main/java/com/example/peaceful_land/DTO/ChangePostThumbnailRequest.java
@@ -1,11 +1,12 @@
 package com.example.peaceful_land.DTO;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
-@Data @Getter @Setter
+@Data @Getter @Setter @Builder
 public class ChangePostThumbnailRequest {
     private Long post_id;
     private MultipartFile image;
